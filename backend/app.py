6d931168913d39b4852a74304381fabf63909ee7
Mikko Tianen <mikko.masala@gmail.com>
2018-10-08 17:59:24 +0300

    some initial setup

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/")
 def hello():
     return "Hello World! pupup"
+
+@app.route("/api/ping")
+def ping():
+  return "pong"
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/ping', { headers: ['Access-Control-Allow-Origin'] }).then(res => console.log(res))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Music review generator</h1>
+        <label htmlFor="uploader">Select a song</label>
+        <input id="uploader" type="file" />
       </div>
-    );
+    )
   }
 }
 
