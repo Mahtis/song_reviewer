4186d8634a3c1e53680fd35044b25c96f626a363
Mikko Tianen <mikko.masala@gmail.com>
2018-10-15 14:53:36 +0300

    key mapping

--- backend/app.py
+++ backend/app.py
@@ -33,12 +33,41 @@ def get_json():
   print('executing')
   with open('test.json') as data_file:    
     data = json.load(data_file)
-  
+  print(data['lowlevel']['average_loudness'])
+  row = []
+  row.append(data['lowlevel']['spectral_complexity']['mean'])
+  row.append(data['lowlevel']['average_loudness'])
+  row.append(data['lowlevel']['dissonance']['mean'])
+  row.append(data['lowlevel']['pitch_salience']['mean'])
+  row.append(data['tonal']['tuning_frequency'])
+  row.append(data['tonal']['chords_strength']['mean'])
+  row.append(data['rhythm']['bpm'])
+  row.append(data['rhythm']['danceability'])
+  row.append(data['rhythm']['beats_count'])
+  row.append(data['metadata']['audio_properties']['length'])
+  row.append(data['tonal']['chords_key'])
+ 
   #j = json.load('test.json')
   # subprocess.call('./script.sh', shell=True)
   print('called it')
   return jsonify(data)
 
+def map_key(key):
+  # A#, C, D, D#, E, F#, G, G# 
+  keys = {
+    'A#': [1, 0, 0, 0, 0, 0, 0, 0],
+    'C': [0, 1, 0, 0, 0, 0, 0, 0],
+    'D': [0, 0, 1, 0, 0, 0, 0, 0],
+    'D#': [0, 0, 0, 1, 0, 0, 0, 0],
+    'E': [0, 0, 0, 0, 1, 0, 0, 0],
+    'F#': [0, 0, 0, 0, 0, 1, 0, 0],
+    'G': [0, 0, 0, 0, 0, 0, 1, 0],
+    'G#': [0, 0, 0, 0, 0, 0, 0, 1]
+  }
+  if keys(key):
+    return keys(key)
+  else:
+    return [0, 0, 0, 0, 0, 0, 0, 0]
 
 """
 @app.route('/', defaults={'path': ''})
