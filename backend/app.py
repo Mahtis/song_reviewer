6b4ecf0d858b604fce9fa95d275310c7a5f1b7a1
Mikko Tianen <mikko.masala@gmail.com>
2018-10-15 14:36:39 +0300

    individual dockerfiles for front and back. Some front back development

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,25 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from flask_cors import CORS
 import subprocess
+import os
+import json
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
+
+if os.getenv('ENV') == 'production':
+  # import to use the python library
+  print('hallo')
+else:
+  print('hello')
+  # import to use the docker container
 
 @app.route("/api/hello")
 def hello():
-  return "Hello World!"
+  print(os.getenv('HELLO'))
+  return 'hello'
 
 @app.route("/api/ping")
 def ping():
@@ -16,10 +28,23 @@ def ping():
   print('called it')
   return "pong"
 
+@app.route("/api/json")
+def get_json():
+  print('executing')
+  with open('test.json') as data_file:    
+    data = json.load(data_file)
+  
+  #j = json.load('test.json')
+  # subprocess.call('./script.sh', shell=True)
+  print('called it')
+  return jsonify(data)
+
+
+"""
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return send_from_directory('./build/', 'index.html')
-
+"""
 if __name__ == '__main__':
     app.run("0.0.0.0", port=8000, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Container, Header, List, Segment } from 'semantic-ui-react'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  state = {
+    song: undefined
+  }
 
   componentDidMount() {
-    axios.get('http://localhost:8000/api/ping').then(res => console.log(res))
+    // axios.get('http://localhost:8000/api/ping').then(res => console.log(res))
+  }
+
+  getJson = () => {
+    axios.get('http://localhost:8000/api/json').then(res => this.setState({ song: res.data }))
   }
 
   render() {
+    const { song } = this.state
+    console.log(song)
     return (
-      <div>
-        <h1>Music review generator</h1>
+      <Container>
+        <Header as="h1">Music review generator</Header>
         <label htmlFor="uploader">Select a song</label>
         <input id="uploader" type="file" />
-      </div>
+        <Button content="submit" onClick={this.getJson}/>
+        {song ?
+        <div>
+          <Header as="h2" content="Attributes of your song" /> 
+          <List>
+            <List.Item>average_loudness: {song.lowlevel.average_loudness.toFixed(2)}</List.Item>
+            <List.Item>dissonance mean: {song.lowlevel.dissonance.mean.toFixed(2)}</List.Item>
+            <List.Item>dynamic_complexity: {song.lowlevel.dynamic_complexity.toFixed(2)}</List.Item>
+            <List.Item>pitch_salience mean: {song.lowlevel.pitch_salience.mean.toFixed(2)}</List.Item>
+            <List.Item>spectral_complexity mean: {song.lowlevel.spectral_complexity.mean.toFixed(2)}</List.Item>
+
+            <List.Item>chords_scale: {song.tonal.chords_scale}</List.Item>
+            <List.Item>chords_key: {song.tonal.chords_key}</List.Item>
+            <List.Item>tuning_frequency: {song.tonal.tuning_frequency.toFixed(2)}</List.Item>
+            <List.Item>chords_strength mean: {song.tonal.chords_strength.mean.toFixed(2)}</List.Item>
+            <List.Item>chords_changes_rate: {song.tonal.chords_changes_rate.toFixed(2)}</List.Item>
+
+            <List.Item>bpm: {song.rhythm.bpm.toFixed(2)}</List.Item>
+            <List.Item>danceability: {song.rhythm.danceability.toFixed(2)}</List.Item>
+            <List.Item>beats_count: {song.rhythm.beats_count.toFixed(2)}</List.Item>
+            
+            <List.Item>audio_properties length: {song.metadata.audio_properties.length.toFixed(2)}</List.Item>
+          </List>
+          <Header as="h2" content="Song review" />
+          <Header as="h3" content="Score: 5/5" />
+          <Segment>
+            This song is definately _____. The sound is really ____.
+          </Segment>
+          <Header as="h3" content="Estimated popularity: 500k views on Youtube" />
+        </div>
+        : undefined}
+      </Container>
     )
   }
 }
