819655d79d0b9df4b48a48a20c9184488b6ab043
Mikko Tianen <mikko.masala@gmail.com>
2018-10-22 14:02:50 +0300

    Song uploading works

--- backend/data_wrangle/model_testing.py
+++ backend/data_wrangle/model_testing.py
@@ -5,85 +5,102 @@ Created on Sat Oct 13 12:46:28 2018
 """
 
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 import numpy as np
-import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import model_selection, feature_selection
-from sklearn.grid_search import GridSearchCV
-import pickle
-plt.style.use('seaborn-deep')
+# from sklearn import model_selection, feature_selection
+# from sklearn.grid_search import GridSearchCV
+# import pickle
+from sklearn.externals import joblib
 
-#read data from csv
-df = pd.read_csv('data_wrangle/df_final.csv')
+# plt.style.use('seaborn-deep')
+class ReviewModel:
+    def __init__(self):
+        self.filename = 'song_review_rtc_model.joblib'
+        self.model = self.get_model(self.filename)
 
-#drop reviews that have no score
-df = df.dropna(subset = ['overall']) 
-df['overall'] = df['overall'].astype(int)
+    def fit_model(self):
+        #read data from csv
+        df = pd.read_csv('data_wrangle/df_final.csv')
 
-#create dummy variables for chords_scale and chords_key
-dummy = pd.get_dummies(df['chords_scale'])
-df = pd.concat([df, dummy], axis=1)
+        #drop reviews that have no score
+        df = df.dropna(subset = ['overall']) 
+        df['overall'] = df['overall'].astype(int)
 
-dummy = pd.get_dummies(df['chords_key'])
-df = pd.concat([df, dummy], axis=1)
+        #create dummy variables for chords_scale and chords_key
+        dummy = pd.get_dummies(df['chords_scale'])
+        df = pd.concat([df, dummy], axis=1)
 
-columns = ['spectral_complexity', 'average_loudness', 'dissonance', 'pitch_salience', 
-'dynamic_complexity','tuning_frequency', 
-'chords_strength', 'chords_changes_rate', 'bpm', 'danceability', 'beats_count', 'length', 'A', 'A#', 'B','C','C#','D','D#','E','F','F#','G','G#','minor','major']
+        dummy = pd.get_dummies(df['chords_key'])
+        df = pd.concat([df, dummy], axis=1)
 
-df = shuffle(df)
-y = df['overall']
-x = df[columns]
+        columns = ['spectral_complexity', 'average_loudness', 'dissonance', 'pitch_salience', 
+        'dynamic_complexity','tuning_frequency', 
+        'chords_strength', 'chords_changes_rate', 'bpm', 'danceability', 'beats_count', 'length', 'A', 'A#', 'B','C','C#','D','D#','E','F','F#','G','G#','minor','major']
 
-# muuttujien P-arvojen tarkastelua. näitä käyttämällä n 1% parannus scoreen logregillä ja rndtreellä
+        df = shuffle(df)
+        y = df['overall']
+        x = df[columns]
 
-p_values = pd.DataFrame()
-p_values['column']= columns
-p_values['P']=feature_selection.f_regression(x, y)[1]
-p_values['F']=feature_selection.f_regression(x, y)[0]
+        # muuttujien P-arvojen tarkastelua. näitä käyttämällä n 1% parannus scoreen logregillä ja rndtreellä
 
-good_cols=[]
-for i,row in p_values.iterrows():
-    if row.P < 0.05:
-        print(row.column,row.P)
-        good_cols.append(row.column)
+        p_values = pd.DataFrame()
+        p_values['column']= columns
+        p_values['P']=feature_selection.f_regression(x, y)[1]
+        p_values['F']=feature_selection.f_regression(x, y)[0]
 
-y2 = df['overall']
-x2 = df[good_cols]
+        good_cols=[]
+        for i,row in p_values.iterrows():
+            if row.P < 0.05:
+                print(row.column,row.P)
+                good_cols.append(row.column)
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(x2,y2,test_size=0.2)
+        y2 = df['overall']
+        x2 = df[good_cols]
 
-
-# param_grid = { "n_estimators"      : [10, 200],
-#            "criterion"         : ["gini"],
-#            "max_features"      : [3, 5],
-#            "max_depth"         : [10, 20],
-#            "min_samples_split" : [2, 4] ,
-#            "bootstrap": [True, False]}
-# grid_search = GridSearchCV(rtc, param_grid, n_jobs=-1, cv=2)
-# grid_search.fit(X_train, y_train)
-# print("grid search best params: ", grid_search.best_params_)
-# grid search best params:  
-# {}
-# print("grid search best score ", grid_search.score(X_test, y_test))
-
-#### rfc with best params ####
-rtc_best = RandomForestClassifier(class_weight='balanced', bootstrap=True, criterion= 'gini', max_depth= 20, 
-max_features= 5, min_samples_split=4, n_estimators= 200)
-
-rtc_best_model = rtc_best.fit(X_train,y_train)
-print(rtc_best_model.score(X_test,y_test))
-result3=pd.DataFrame(rtc_best_model.predict(X_test))
-result3.plot.hist()
-print(result3[0].value_counts(),result3[0].value_counts(normalize=True))
-
-filename = 'song_review_rtc_model.sav'
-pickle.dump(rtc_best_model, open(filename, 'wb'))
-
-########
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(x2,y2,test_size=0.2)
 
 
-plt.show()
+        # param_grid = { "n_estimators"      : [10, 200],
+        #            "criterion"         : ["gini"],
+        #            "max_features"      : [3, 5],
+        #            "max_depth"         : [10, 20],
+        #            "min_samples_split" : [2, 4] ,
+        #            "bootstrap": [True, False]}
+        # grid_search = GridSearchCV(rtc, param_grid, n_jobs=-1, cv=2)
+        # grid_search.fit(X_train, y_train)
+        # print("grid search best params: ", grid_search.best_params_)
+        # grid search best params:  
+        # {}
+        # print("grid search best score ", grid_search.score(X_test, y_test))
+
+        #### rfc with best params ####
+        rtc_best = RandomForestClassifier(class_weight='balanced', bootstrap=True, criterion= 'gini', max_depth= 20, 
+        max_features= 5, min_samples_split=4, n_estimators= 200)
+
+        model = rtc_best.fit(X_train,y_train)
+        print(model.score(X_test,y_test))
+        # result3=pd.DataFrame(rtc_best_model.predict(X_test))
+        # result3.plot.hist()
+        # print(result3[0].value_counts(),result3[0].value_counts(normalize=True))
+
+        joblib.dump(model, self.filename) 
+        # pickle.dump(rtc_best_model, open(filename, 'wb'))
+        return model
+
+    ########
+
+    def get_model(self, filename):
+        try:
+            model = joblib.load(filename)
+        except FileNotFoundError:
+            model = self.fit_model()
+        # model = pickle.load(filename, 'rb')
+        return model
+
+    def predict(self, row):
+        return self.model.predict([row])
+
+# plt.show()
         
--- backend/script.sh
+++ backend/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "I FEEL SO ALIVE"
-docker run -ti --rm -v `pwd`:/essentia mtgupf/essentia essentia_streaming_extractor_music 06.wav test.json
+docker run -ti --rm -v `pwd`:/essentia mtgupf/essentia essentia_streaming_extractor_music $1 $2
+echo "I am done"
